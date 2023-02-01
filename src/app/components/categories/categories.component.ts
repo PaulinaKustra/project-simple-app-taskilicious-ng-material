ddd835583493e009a43a79d99424d3dc79ba59e3
Paulina Kustra <118442022+PaulinaKustra@users.noreply.github.com>
2023-02-01 08:37:12 +0100

    category sorting fixed

--- src/app/components/categories/categories.component.ts
+++ src/app/components/categories/categories.component.ts
@@ -1,59 +1,41 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import {map, Observable, of, Subject} from 'rxjs';
-import { CategoryModel } from '../../models/category.model';
-import { CategoryService } from '../../services/category.service';
+import {ChangeDetectionStrategy, Component, ViewEncapsulation} from '@angular/core';
+import {BehaviorSubject, combineLatest, map, Observable, of} from 'rxjs';
+import {CategoryModel} from '../../models/category.model';
+import {CategoryService} from '../../services/category.service';
 import {Router} from "@angular/router";
 import {FormControl, Validators} from "@angular/forms";
 
 @Component({
-  selector: 'app-categories',
-  styleUrls: ['./categories.component.scss'],
-  templateUrl: './categories.component.html',
-  encapsulation: ViewEncapsulation.Emulated,
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'app-categories',
+    styleUrls: ['./categories.component.scss'],
+    templateUrl: './categories.component.html',
+    encapsulation: ViewEncapsulation.Emulated,
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CategoriesComponent {
-  sortingForm = new FormControl('A-Z', [Validators.required])
-  data$: Observable<CategoryModel[]> = this._categoryService.getAll();
+    private readonly _ascFilter = "A-Z";
+    private readonly _descFilter = "Z-A"
+    private _data$: Observable<CategoryModel[]> = this._categoryService.getAll();
+    public sortingData$: Observable<string[]> = of([this._ascFilter, this._descFilter]);
+    public sortingForm = new FormControl(this._ascFilter, [Validators.required])
+    private _sortSubject: BehaviorSubject<string> = new BehaviorSubject<string>(this._ascFilter);
 
-  private _sortSubject: Subject<string> = new Subject<string>();
+    public  filteredData$ = combineLatest([this._data$, this._sortSubject]).pipe(
+        map(([data, filter]) => {
+            data.sort((a, b) => {
+                if (filter === this._ascFilter)
+                    return a.name.toUpperCase() < b.name.toUpperCase() ? -1 : 1;
+                else
+                    return a.name.toUpperCase() > b.name.toUpperCase() ? -1 : 1;
+            });
+            return data;
+        })
+    )
 
-  constructor(private _categoryService: CategoryService, private _router: Router) {
-    this._sortSubject.subscribe((sortType) => {
+    constructor(private _categoryService: CategoryService, private _router: Router) {
+    }
 
-      this.data$ = this.data$.pipe(map((data) => {
-        data.sort((a, b) => {
-          if (sortType === "A-Z")
-            return a.name.toUpperCase() < b.name.toUpperCase() ? -1 : 1;
-          else
-            return a.name.toUpperCase() > b.name.toUpperCase() ? -1 : 1;
-        });
-        return data;
-      }))
-    })
-
-  }
-
-  ngOnDestroy(): void {
-    this._sortSubject.unsubscribe();
-  }
-
-  sortingData$: Observable<string[]> = of(["A-Z", "Z-A"]);
-
-
-
-  onSortChange(sortType: any) {
-    this._sortSubject.next(sortType);
-    // console.log("event: ", sortType)
-    //
-    // this.data$ = this.data$.pipe(map((data) => {
-    //   data.sort((a, b) => {
-    //     if (sortType === "A-Z")
-    //       return a.name.toUpperCase() < b.name.toUpperCase() ? -1 : 1;
-    //     else
-    //       return a.name.toUpperCase() > b.name.toUpperCase() ? -1 : 1;
-    //   });
-    //   return data;
-    // }))
-  }
+    onSortChange(sortType: any) {
+        this._sortSubject.next(sortType);
+    }
 }
